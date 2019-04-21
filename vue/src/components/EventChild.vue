629ca5349f9858a6841c4fd92233ae42aa3d9823
hardlitchi <hardlitchi@gmail.com>
2019-04-21 19:42:58 +0900

    vue sample

---
+++ vue/src/components/EventChild.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>{{ id }} : {{ name }}</li>
+</template>
+
+<script>
+export default {
+  name: 'EventChild',
+  props: ['id', 'name']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue/src/components/ProjectBoard.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="board">
+    <h1>{{ name }}</h1>
+    <ul>
+      <li> test : testcon</li>
+      <EventChild v-for="task in tasks"
+        v-bind:key="task.id"
+        v-bind:id="task.id"
+        v-bind:name="task.name"></EventChild>
+    </ul>
+  </div>
+</template>
+
+<script>
+import EventChild from '@/components/EventChild'
+
+export default {
+  name: 'ProjectBoard',
+  data () {
+    return {
+      name: 'Your project name.',
+      tasks: [
+        {id: 1, name: '春のパン祭り'},
+        {id: 2, name: '穴滝'},
+        {id: 3, name: 'ストリートスナップ'}
+      ]
+    }
+  },
+  components: {
+    EventChild
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
